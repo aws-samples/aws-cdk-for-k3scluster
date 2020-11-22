d702a9e78c9bf85666e553d4f17f8d44ac6da55e
Pahud Hsieh <pahudnet@gmail.com>
2020-11-22 23:24:06 +0000

    chore(upgrade): add ProjenYarnUpgrade workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,5 +1,5 @@
 const {
-  AwsCdkConstructLibrary
+  AwsCdkConstructLibrary, GithubWorkflow
 } = require('projen');
 
 const AWS_CDK_LATEST_RELEASE = '1.62.0';
@@ -50,6 +50,47 @@ const project = new AwsCdkConstructLibrary({
     distName: 'cdk-k3s-cluster',
     module: 'cdk_k3s_cluster'
   }
+});
+
+
+// create a custom projen and yarn upgrade workflow
+const workflow = new GithubWorkflow(project, 'ProjenYarnUpgrade');
+
+workflow.on({
+  schedule: [{
+    cron: '0 6 * * 0'
+  }], // 6AM every Sunday
+  workflow_dispatch: {}, // allow manual triggering
+});
+
+workflow.addJobs({
+  upgrade: {
+    'runs-on': 'ubuntu-latest',
+    'steps': [
+      { uses: 'actions/checkout@v2' },
+      { 
+        uses: 'actions/setup-node@v1',
+        with: {
+          'node-version': '10.17.0',
+        }
+      },
+      { run: `yarn upgrade` },
+      { run: `yarn projen:upgrade` },
+      // submit a PR
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v3',
+        with: {
+          'token': '${{ secrets.PROJEN_GITHUB_TOKEN }}',
+          'commit-message': 'chore: upgrade projen',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade projen and yarn',
+          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
+          'labels': 'auto-merge',
+        }
+      },
+    ],
+  },
 });
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
