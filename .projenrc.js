65981b9e10dd85198ef47c2862141301ab5ff0d8
Pahud Hsieh <pahudnet@gmail.com>
2021-06-05 00:08:02 +0000

    chore(projen): bump version

--- .projenrc.js
+++ .projenrc.js
@@ -9,7 +9,7 @@ const PROJECT_DESCRIPTION = 'A JSII construct lib to deploy a K3s cluster on AWS
 const project = new AwsCdkConstructLibrary({
   authorName: 'Massimo Re Ferre',
   authorEmail: 'mreferre@amazon.com',
-  name: 'cdk-k3s-cluster',
+  name: PROJECT_NAME,
   repository: 'https://github.com/aws-samples/aws-cdk-for-k3scluster',
   description: PROJECT_DESCRIPTION,
   license: 'MIT',
@@ -23,7 +23,6 @@ const project = new AwsCdkConstructLibrary({
     'spot',
   ],
   dependabot: false,
-  releaseBranches: ['master'],
   defaultReleaseBranch: 'master',
   catalog: {
     twitter: 'mreferre',
@@ -43,47 +42,6 @@ const project = new AwsCdkConstructLibrary({
   python: {
     distName: 'cdk-k3s-cluster',
     module: 'cdk_k3s_cluster',
-  },
-});
-
-
-// create a custom projen and yarn upgrade workflow
-const workflow = project.github.addWorkflow('ProjenYarnUpgrade');
-
-workflow.on({
-  schedule: [{
-    cron: '0 6 * * 0',
-  }], // 6AM every Sunday
-  workflow_dispatch: {}, // allow manual triggering
-});
-
-workflow.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.PROJEN_GITHUB_TOKEN }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
   },
 });
 
